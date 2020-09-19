052fdeb79a8b56703dc049b565a2809a591e7810
petrero <petrero@gmail.com>
2020-09-19 14:51:37 +0300

    4.2. Mutations and actions -- Write mutations and actions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js';
 
 Vue.use(Vuex)
 
@@ -14,10 +15,20 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
-    ]
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({commit}, event){
+      EventService.postEvent(event)
+      commit('ADD_EVENT', event)
+    }
+  },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)

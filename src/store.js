97cf0e56740529a8d911c1abe797f995709d5e51
petrero <petrero@gmail.com>
2020-09-19 14:40:37 +0300

    4.1 Mutations and actions -- Add a form to EventCreate in order to create an event

--- src/store.js
+++ src/store.js
@@ -14,32 +14,11 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
-    ],
-    todos: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false },
-      { id: 3, text: '...', done: true },
-      { id: 4, text: '...', done: false }
-    ],
-    events: [
-      {id: 1, title: '...', organizer: '...'},
-      {id: 2, title: '...', organizer: '...'},
-      {id: 3, title: '...', organizer: '...'},
-      {id: 4, title: '...', organizer: '...'}
     ]
   },
   mutations: {},
   actions: {},
   getters: {
-    catLength: state => {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: state => {
-      return state.todos.filter(todo => !todo.done).length
-    },
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
     }

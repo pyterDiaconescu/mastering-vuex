fe7ead90520e514310c7848630793575f0fd1549
petrero <petrero@gmail.com>
2020-09-19 11:30:58 +0300

    3.9. State and Getters -- Map getters to computed property using mapGetters vuex provided function

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,21 +3,16 @@
     <h1>Create an event, {{ user.name }}</h1>
     <p>This was an event created by {{ user.id }}</p>
     <p>There are {{ catLength }} categories</p>
-    <p>{{ getEvent(1) }}</p>
+    <p>{{ getEventById(2) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   computed: {
-    catLength() {
-      return this.$store.getters.catLength
-    },
-    getEvent(){
-      return this.$store.getters.getEventById
-    },
+    ...mapGetters(['getEventById']),
     ...mapState([
       'user',
       'categories'

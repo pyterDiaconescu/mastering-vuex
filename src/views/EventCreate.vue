cc66d828fe607991ef90610931c7f2e420f6911d
petrero <petrero@gmail.com>
2020-09-21 09:48:05 +0300

    7.4. Success and error notification -- Code in EventCreate component on catch branch is no longer needed

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -65,9 +65,7 @@ export default {
             params: { id: this.event.id }
           })
           this.event = this.createFreshEventObject()
-        }).catch(() => {
-          console.log('There was a problem creating your event')
-        })
+        }).catch(() => {})
     },
     createFreshEventObject(){
       const user = this.$store.state.user.user

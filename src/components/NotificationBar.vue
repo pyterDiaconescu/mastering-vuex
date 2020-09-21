b7f16e6cdb500c84b443d2d7b4c658231e62df78
petrero <petrero@gmail.com>
2020-09-21 09:51:09 +0300

    7.5. Success and error notification -- Create notification component to display notification messages

---
+++ src/components/NotificationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notification-bar">
+    <p>{{ notification.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      required: true
+    },
+  },
+}
+</script>
+
+<style scoped>
+ .notification-bar {
+   margin: 1em 0 1em;
+ }
+</style>

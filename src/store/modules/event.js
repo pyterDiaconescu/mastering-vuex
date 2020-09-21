6e32e493ca46f41f6f2256f00e7f5d9712ee0168
petrero <petrero@gmail.com>
2020-09-21 08:43:47 +0300

    7.3. Success and error notification -- Send success or error notificaton to store when create an event. Throw error in catch in order to propagate the error up to component

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -23,9 +23,21 @@ export const  mutations = {
 }
 
 export const actions = {
-  createEvent({commit}, event){
+  createEvent({commit, dispatch}, event){
     return EventService.postEvent(event).then(() => {
       commit('ADD_EVENT', event)
+      const notification = {
+        type: 'success',
+        message: 'Your event has been created!'
+      }
+      dispatch('notification/add', notification, { root: true })
+    }).catch(error => {
+      const notification = {
+        type: error,
+        message: 'There was a problem creating your event: ' + error.message
+      }
+      dispatch('notification/add', notification, { root: true })
+      throw error
     })
   },
   fetchEvents({ commit, dispatch }, { perPage, page }){
